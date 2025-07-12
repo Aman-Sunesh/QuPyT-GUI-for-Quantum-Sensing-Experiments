0d4f7b2e9033c43c33636bcbcdb9f87ac19b848d
Aman Sunesh <157351006+Aman-Sunesh@users.noreply.github.com>
2025-07-12 06:58:25 +0000

    Initial commit of all project files

---
+++ Version 1/stop_pb.py
@@ -0,0 +1,17 @@
+import spinapi
+
+BOARD = 0
+
+def stop_pulse_blaster():
+    rv = spinapi.pb_init()
+    if rv != 0:
+        print(f"Warning: pb_init() returned {rv} – continuing anyway")
+    spinapi.pb_select_board(BOARD)
+    spinapi.pb_stop()
+
+    try:
+        spinapi.pb_reset()
+    except AttributeError:
+        pass
+
+    spinapi.pb_close()
